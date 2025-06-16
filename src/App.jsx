b65e5a11a3ef904d8d0c5c83db49bfba58fb78d4
AndiswaCyria <andiswacyriam@gmail.com>
2025-06-16 16:03:38 +0200

    Initial commit from create-react-router

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greeting from './components/Greeting.jsx';
+
+function App() {
+    return (
+        <div>
+            <Greeting name="Ray" />
+        </div>
+    );
+}
+
+export default App;
